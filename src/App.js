b5baf4989b8acaa8f9ad5ed4a015022340389728
bursalma <bursalma@gmail.com>
2020-04-20 16:22:13 -0700

    sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Homepage from './components/Homepage/Homepage'
+
 function App() {
   return (
     <div>
-      Hello World
+      <Homepage/>
     </div>
   );
 }
---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Homepage.scss'
+
+const Homepage = () => (
+    <div className='Homepage'>
+        <div className='DirectoryMenu'>
+            <div className='MenuItem'>
+                <div className='Content'>
+                    <h1 className='Title'>
+                        HATS
+                    </h1>
+                    <span className='Subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='MenuItem'>
+                <div className='Content'>
+                    <h1 className='Title'>
+                        JACKETS
+                    </h1>
+                    <span className='Subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='MenuItem'>
+                <div className='Content'>
+                    <h1 className='Title'>
+                        SNEAKERS
+                    </h1>
+                    <span className='Subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='MenuItem'>
+                <div className='Content'>
+                    <h1 className='Title'>
+                        WOMEN'S
+                    </h1>
+                    <span className='Subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className='MenuItem'>
+                <div className='Content'>
+                    <h1 className='Title'>
+                        MEN'S
+                    </h1>
+                    <span className='Subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
