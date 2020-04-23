028d372db24ff871bacac14da9161db25aa8b225
bursalma <bursalma@gmail.com>
2020-04-23 16:35:12 -0700

    FormInput Component

---
+++ src/components/FormInput/FormInput.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './FormInput.scss';
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+    <div className='group'>
+        <input 
+            className='FormInput' 
+            onChange={handleChange} 
+            {...otherProps} 
+        />
+        {
+            label ?
+            <label 
+                className={`${
+                    otherProps.value.length ? 'shrink' : ''
+                } formInputLabel`}
+            >
+                {label}
+            </label>
+            : null
+        }
+    </div>
+);
+
+export default FormInput;
