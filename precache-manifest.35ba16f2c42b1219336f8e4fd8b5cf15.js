5854c367bc8ae80c00878db300385212810e677e
bursalma <bursalma@gmail.com>
2020-05-06 18:50:49 -0700

    Updates

---
+++ precache-manifest.35ba16f2c42b1219336f8e4fd8b5cf15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f85b66adedd686b92a6ff7b19dff66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60561f819c019a57930a",
+    "url": "/static/css/main.5c06bf6d.chunk.css"
+  },
+  {
+    "revision": "e56b3eb4a368ed0b23d8",
+    "url": "/static/js/2.9959b2fd.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.9959b2fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60561f819c019a57930a",
+    "url": "/static/js/main.a34f6e75.chunk.js"
+  },
+  {
+    "revision": "a8202d2c482926c65fe9",
+    "url": "/static/js/runtime-main.f3d78015.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/original.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-commerce/precache-manifest.3908d7d5c2d6310afd9e27b4f0711016.js"
+  "/precache-manifest.35ba16f2c42b1219336f8e4fd8b5cf15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-commerce/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
